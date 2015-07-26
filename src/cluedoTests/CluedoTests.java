0b448d230281eb6033e729a21686e110e3fb2ff5
TanaTanoi <tana.tanoi@gmail.com>
2015-07-26 17:48:47 +1200

    Added test class
    -Hasn't got any content yet, will update closer to completion.

---
+++ src/cluedoTests/CluedoTests.java
@@ -0,0 +1,21 @@
+package cluedoTests;
+
+import org.junit.*;
+
+import cluedoPieces.*;
+
+public class CluedoTests {
+	
+/*
+ *---------------------------
+ *------Deck Class tests-----
+ *--------------------------- 
+ */
+	Deck deck;
+	@Test
+	public void deck_create_1(){
+		deck = new Deck();
+	}
+	
+	
+}
