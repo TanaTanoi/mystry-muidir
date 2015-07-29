25f78c953c7f0a950aea457163b3f41f429b9782
CDeighton <cullumdeighton@gmail.com>
2015-07-29 22:09:41 +1200

    still testing. lord save me.

--- src/cluedoGame/CluedoGame.java
+++ src/cluedoGame/CluedoGame.java
@@ -18,7 +18,7 @@ public class CluedoGame {
 		//System.out.println(ui.requestPoint("Enter a point.","Not a valid point!"));
 		Board b = new Board();
 		while(true){
-			System.out.println("many overfed cats");
+			System.out.println("many overfed dogs");
 		}
 	}
 
